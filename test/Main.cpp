651ac0d3be1fb4f7d668cbc647af53463357319c
Uwe <uwe@runtemund.de>
2023-11-01 13:36:32 +0100

    Restore Main.cpp of test app.

--- test/Main.cpp
+++ test/Main.cpp
@@ -5,7 +5,7 @@
 //  Created by Uwe Runtemund on 17.01.13.
 //  Copyright (c) 2013 Jameo Software. All rights reserved.
 //
-/*
+
 #include "core/Core.h"
 
 #include "core/GeometryTest.h"
@@ -48,51 +48,4 @@ int main( int argc, const char* argv[] )
 
 	System::Quit();
 	return result.Int32();
-}
-*/
-
-#include "core/Core.h"
-
-// Example test class to show simple test anatomy.
-class ExampleTest : public jm::Test
-{
-public:
-
-	// Constructor
-	ExampleTest() :Test()
-	{
-		SetName("Example Test");
-	};
-
-	// Mandatory overriden test method. The heart of the test!
-	void DoTest() override
-	{
-		int32 a = 2;
-		int32 b = 5;
-		TestEquals(a, b, "a is not b");
-		a = 5;
-		TestEquals(a, b, "a is not b");
-	};
-};
-
-// Entry methof of test application
-int main(int argc, const char* argv[])
-{
-	// Init Jameo System
-	jm::System::Init();
-	jm::System::Log("Example Test Suite", jm::kLogInformation);
-
-	// Init test vector
-	jm::TestVector* vec = new jm::TestVector(argc, argv);
-	vec->AddTest(new ExampleTest());
-
-	// Execute test vector
-	int32 result = vec->Execute().Int32();
-
-	// Clean up
-	delete vec;
-	jm::System::Quit();
-
-	// Return the number of errors to help calling scripts
-	return result;
 }
